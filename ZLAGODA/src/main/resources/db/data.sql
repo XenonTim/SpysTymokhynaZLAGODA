3518f0300e6ce7c16ac13e992b8a8f4d392e9a41
XenonTim <xeny.timokhina@gmail.com>
2026-06-04 19:46:44 +0300

    commit project structure and DB

---
+++ ZLAGODA/src/main/resources/db/data.sql
@@ -0,0 +1,93 @@
+INSERT INTO Category (category_name,category_number) VALUES
+('meat',1),
+('dairy products',2),
+('seafood',3),
+('alcohol',4),
+('soft drinks',5),
+('fruits',6),
+('vegetables',7),
+('dry goods',8),
+('sweets',9),
+('baked goods',10);
+
+INSERT INTO Customer_Card (cust_surname,cust_name,cust_patronymic,phone_number,city,street,zip_code,percent,card_number) VALUES
+('Petrenko','Svitlana','Olehivna','+380671002030','Kyiv','Baumana','10068',30,1001),
+('Sydorenko','Petro','Pavlovych','+380932003040','Lviv','Lisozakhysna','10937',15,1002),
+('Vasylenko','Hanna','Antonivna','+380503004050','Odesa','Avtozavodskyi','10003',25,1003),
+('Lysenko','Mykhailo','Fedorovych','+380984005060','Dnipro','Saliutna','11495',20,1004),
+('Pavlenko','Yuliia','Viktorivna','+380676007080','Kyiv','Yalynkovyi','12067',15,1005),
+('Romaniuk','Artem','Leonidovych','+380957008090','Vinnytsia','Hostomelska','10375',25,1006),
+('Kozak','Olena','Stepanivna','+380688009000','Poltava','Sorochynska','11580',15,1007),
+('Danyliuk','Ihor','Mykhailovych','+380939000111','Chernihiv','Cherkaska','11858',10,1008),
+('Stepaniuk','Iryna','Borysivna','+380500112233','Cherkasy','Basein','10062',25,1009),
+('Melnyk','Oleh','Serhiiovych','+380635006074','Lviv','Svaromska','12040',5,1010);
+
+INSERT INTO Employee (empl_surname,empl_name,empl_patronymic,empl_role,salary,date_of_birth,date_of_start,phone_number,city,street,zip_code,id_employee) VALUES
+('Kovalenko','Ivan','Mykolaiovych','Manager',25000,'1990-05-15 00:00:00.000000','2020-01-10 00:00:00.000000','+380971112233','Kyiv','Umanska','11735',101),
+('Melnyk','Olha','Petrivna','Cashier',22000,'1995-08-20 00:00:00.000000','2021-03-15 00:00:00.000000','+380932223344','Kyiv','Harazhna','10304',102),
+('Tkachenko','Andrii','Ivanovych','Cashier',19000,'1988-12-02 00:00:00.000000','2019-11-05 00:00:00.000000','+380503334455','Kyiv','Trukhanivska','11711',103),
+('Shevchenko','Mariia','Vasylivna','Cashier',21000,'1992-03-25 00:00:00.000000','2022-06-01 00:00:00.000000','+380674445566','Kyiv','Borova','10184',104),
+('Bondarenko','Serhii','Oleksandrovych','Cashier',19500,'1997-07-10 00:00:00.000000','2023-01-20 00:00:00.000000','+380985556677','Kyiv','Telihy Oleny','11663',105),
+('Kravchenko','Olena','Dmytrivna','Manager',23000,'1991-11-30 00:00:00.000000','2020-09-12 00:00:00.000000','+380636667788','Kyiv','Zelena','12449',106),
+('Oliinyk','Viktor','Serhiiovych','Cashier',20000,'1985-04-14 00:00:00.000000','2018-05-25 00:00:00.000000','+380507778899','Kyiv','Shosta','12573',107),
+('Polishchuk','Anna','Ihorivna','Cashier',22500,'1994-09-05 00:00:00.000000','2021-10-10 00:00:00.000000','+380678889900','Kyiv','Honcharna','10353',108),
+('Savchenko','Tetiana','Volodymyrivna','Cashier',18500,'1999-01-18 00:00:00.000000','2024-02-01 00:00:00.000000','+380939990011','Kyiv','Dachna','12636',109),
+('Moroz','Dmytro','Yuriiovych','Manager',24000,'1989-06-22 00:00:00.000000','2019-02-28 00:00:00.000000','+380980001122','Kyiv','Yasenevyi','11961',110);
+
+INSERT INTO Product (category_number,id_product,product_name,characteristics) VALUES
+(1,101,'Chicken drumsticks','1kg'),
+(2,102,'Milk','900ml, 2.5%, ultra pasterised'),
+(3,103,'Salmon fillet','200g'),
+(4,104,'Red wine','1l, 11%'),
+(5,105,'Sparkling water','250ml'),
+(6,106,'Bananas','imported from Equador'),
+(7,107,'Tomatoes','domestic'),
+(8,108,'Spagheti','400g'),
+(9,109,'Chocolate','90g, dark with nuts'),
+(10,110,'Bread','350g, sliced');
+
+INSERT INTO Store_Product (UPC_prom,UPC,id_product,selling_price,products_number,promotional_product) VALUES
+(1000000001,1000000001,101,109,32,true),
+(1000000002,1000000002,102,70,100,true),
+(NULL,1000000003,103,167,14,false),
+(1000000004,1000000004,104,95,54,true),
+(NULL,1000000005,105,45,48,false),
+(NULL,1000000006,106,74,23,false),
+(NULL,1000000007,107,190,90,false),
+(NULL,1000000008,108,55,42,false),
+(NULL,1000000009,109,100,100,false),
+(NULL,1000000010,110,36,72,false);
+
+INSERT INTO `Check` (id_employee,check_number,card_number,print_date,sum_total,vat) VALUES
+(101,10001,1005,'2025-12-11 00:00:00.000000',298,60),
+(102,10002,NULL,'2025-07-22 00:00:00.000000',710,142),
+(103,10003,1006,'2025-11-04 00:00:00.000000',410,82),
+(104,10004,1006,'2026-02-10 00:00:00.000000',374,75),
+(105,10005,1007,'2025-10-13 00:00:00.000000',680,136),
+(106,10006,NULL,'2025-07-22 00:00:00.000000',679,136),
+(107,10007,NULL,'2025-01-16 00:00:00.000000',469,94),
+(108,10008,1010,'2025-06-08 00:00:00.000000',573,115),
+(109,10009,NULL,'2025-03-24 00:00:00.000000',399,80),
+(110,10010,NULL,'2025-01-26 00:00:00.000000',636,127);
+
+INSERT INTO Sale (UPC,check_number,product_number,selling_price) VALUES
+(1000000001,10006,1,109),
+(1000000002,10002,2,70),
+(1000000003,10007,2,167),
+(1000000003,10008,3,167),
+(1000000003,10009,1,167),
+(1000000003,10010,3,167),
+(1000000004,10001,2,95),
+(1000000005,10007,3,45),
+(1000000005,10010,3,45),
+(1000000006,10004,1,74),
+(1000000006,10009,3,74),
+(1000000007,10002,3,190),
+(1000000007,10005,2,190),
+(1000000007,10006,3,190),
+(1000000008,10003,2,55),
+(1000000009,10003,3,100),
+(1000000009,10004,3,100),
+(1000000009,10005,3,100),
+(1000000010,10001,3,36),
+(1000000010,10008,2,36);
---
+++ ZLAGODA/src/main/resources/db/schema.sql
@@ -0,0 +1,87 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS Sale;
+DROP TABLE IF EXISTS Store_Product;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS `Check`;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Customer_Card;
+DROP TABLE IF EXISTS Category;
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE Category (
+category_number INT(11) NOT NULL AUTO_INCREMENT,
+category_name VARCHAR(50) NOT NULL,
+PRIMARY KEY (category_number)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Customer_Card (
+card_number INT(11) NOT NULL AUTO_INCREMENT,
+cust_surname VARCHAR(50) NOT NULL,
+cust_name VARCHAR(50) NOT NULL,
+cust_patronymic VARCHAR(50),
+phone_number VARCHAR(13) NOT NULL,
+city VARCHAR(50),
+street VARCHAR(50),
+zip_code VARCHAR(9),
+percent INT(11) DEFAULT 0 NOT NULL,
+PRIMARY KEY (card_number)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Employee (
+id_employee INT(11) NOT NULL AUTO_INCREMENT,
+empl_surname VARCHAR(50) NOT NULL,
+empl_name VARCHAR(50) NOT NULL,
+empl_patronymic VARCHAR(50),
+empl_role VARCHAR(10) NOT NULL,
+salary DECIMAL(13,2) DEFAULT 0.00 NOT NULL,
+date_of_birth DATETIME NOT NULL,
+date_of_start DATETIME NOT NULL,
+phone_number VARCHAR(13),
+city VARCHAR(50) NOT NULL,
+street VARCHAR(50) NOT NULL,
+zip_code VARCHAR(9) NOT NULL,
+PRIMARY KEY (id_employee)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `Check` (
+check_number INT(11) NOT NULL AUTO_INCREMENT,
+id_employee INT(11) NOT NULL,
+card_number INT(11) DEFAULT NULL,
+print_date DATETIME NOT NULL,
+sum_total DECIMAL(13,2) DEFAULT 0.00 NOT NULL,
+vat DECIMAL(13,2) DEFAULT 0.00 NOT NULL,
+PRIMARY KEY (check_number),
+CONSTRAINT fk_check_employee FOREIGN KEY (id_employee) REFERENCES Employee (id_employee) ON DELETE RESTRICT ON UPDATE CASCADE,
+CONSTRAINT fk_check_customer_card FOREIGN KEY (card_number) REFERENCES Customer_Card (card_number) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Product (
+id_product INT(11) NOT NULL AUTO_INCREMENT,
+category_number INT(11) NOT NULL,
+product_name VARCHAR(50) NOT NULL,
+characteristics VARCHAR(100) NOT NULL,
+PRIMARY KEY (id_product),
+CONSTRAINT fk_product_category FOREIGN KEY (category_number) REFERENCES Category (category_number) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Store_Product (
+UPC INT(11) NOT NULL AUTO_INCREMENT,
+UPC_prom INT(11) DEFAULT NULL,
+id_product INT(11) NOT NULL,
+selling_price DECIMAL(13,2) DEFAULT 0.00 NOT NULL,
+products_number INT(11) DEFAULT 0 NOT NULL,
+promotional_product BOOLEAN DEFAULT false,
+PRIMARY KEY (UPC),
+CONSTRAINT fk_store_product_product FOREIGN KEY (id_product) REFERENCES Product (id_product) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_store_product_prom FOREIGN KEY (UPC_prom) REFERENCES Store_Product (UPC) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Sale (
+UPC INT(11) NOT NULL,
+check_number INT(11) NOT NULL,
+product_number INT(11) DEFAULT 0 NOT NULL,
+selling_price DECIMAL(13,2) DEFAULT 0.00 NOT NULL,
+PRIMARY KEY (UPC, check_number),
+CONSTRAINT fk_sale_store_product FOREIGN KEY (UPC) REFERENCES Store_Product (UPC) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_sale_check FOREIGN KEY (check_number) REFERENCES `Check` (check_number) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
